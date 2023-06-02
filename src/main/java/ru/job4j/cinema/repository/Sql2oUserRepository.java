d140ba63dc65d496d23120a3cdcbb44b61cecee7
Stepan Bogdanov <bogstepan@gmail.com>
2023-06-02 13:01:00 +0300

    Добавил регистрацию

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -0,0 +1,58 @@
+package ru.job4j.cinema.repository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.sql2o.Sql2o;
+import ru.job4j.cinema.model.User;
+
+import java.util.Optional;
+
+@Repository
+public class Sql2oUserRepository implements UserRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class.getName());
+
+    private final Sql2o sql2o;
+
+    public Sql2oUserRepository(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        try (var connection = sql2o.open()) {
+            var sql = """
+                    INSERT INTO users (full_name, email, password) values (:fullName, :email, :password)
+                    """;
+            var query = connection.createQuery(sql, true)
+                    .addParameter("fullName", user.getFullName())
+                    .addParameter("email", user.getEmail())
+                    .addParameter("password", user.getPassword());
+            int generatedId = query.executeUpdate().getKey(Integer.class);
+            user.setId(generatedId);
+            return Optional.of(user);
+        } catch (Exception e) {
+            LOG.error("Registration error", e);
+        }
+        return Optional.empty();    }
+
+    @Override
+    public Optional<User> findByEmailAndPassword(String email, String password) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE email = :email AND password = :password");
+            var user = query.addParameter("email", email).addParameter("password", password)
+                    .setColumnMappings(User.COLUMN_MAPPING).executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (var connection = sql2o.open()) {
+            var query = connection.createQuery("SELECT * FROM users WHERE email = :email");
+            var user = query.addParameter("email", email).setColumnMappings(User.COLUMN_MAPPING)
+                    .executeAndFetchFirst(User.class);
+            return Optional.ofNullable(user);
+        }    }
+}
--- src/main/java/ru/job4j/cinema/service/UserService.java
+++ src/main/java/ru/job4j/cinema/service/UserService.java
@@ -1,15 +1,15 @@
-package ru.job4j.cinema.repository;
+package ru.job4j.cinema.service;
 
 import ru.job4j.cinema.model.User;
 
 import java.util.Optional;
 
-public interface UserRepository {
+public interface UserService {
 
     Optional<User> save(User user);
 
     Optional<User> findByEmailAndPassword(String email, String password);
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findUserByEmail(String email);
 
 }
